a60103a8abcd08dec6c8abef2525a398d57c52a7
ub-victor <victoireushindi371@gmail.com>
2026-01-22 14:54:53 +0200

    Initial commit

--- new-react-app/src/App.jsx
+++ new-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Person = () => {
+const Person = (props) => {
   return(
     <>
     <h1>Name :</h1>
