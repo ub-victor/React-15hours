ab614817f55e3175383fc502911356180fda9c78
ub-victor <victoireushindi371@gmail.com>
2026-01-23 11:44:33 +0200

    Initial commit

--- new-react-app/src/App.jsx
+++ new-react-app/src/App.jsx
@@ -17,11 +17,7 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <button onClick={()=> setCounter((prevCount)=> prevCount -1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=> setCounter((nextCount)=> nextCount +1)} >+</button>
-    </div>
+   <h1>App</h1>
   )
 }
 
