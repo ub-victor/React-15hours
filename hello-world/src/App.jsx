3beb2cec0e5759821268323b3485688835090102
ub-victor <victoireushindi371@gmail.com>
2026-01-26 19:34:47 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Hello Victor</h1>
       <div className="card">
         <button onClick={() => setCount((counts) => counts + 1)}>
-          count is {count}
+          counts is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
