3b67a74b29474ae3bb33a2a40e7db2268c1c5f93
ub-victor <victoireushindi371@gmail.com>
2026-01-26 19:42:18 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -4,30 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello Victor</h1>
-      <div className="card">
-        <button onClick={() => setCount((counts) => counts + 1)}>
-          counts is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
