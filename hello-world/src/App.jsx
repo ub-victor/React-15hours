e57a14afdfd172953a879f4461b70b85fdd13797
ub-victor <victoireushindi371@gmail.com>
2026-01-27 15:46:46 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -7,9 +7,7 @@ function App() {
 
   return (
     <>
-      <Message>
-        <button>Action</button>
-      </Message>
+      <Message/>
     </>
   )
 }
--- hello-world/src/components/Message.jsx
+++ hello-world/src/components/Message.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 const Message = (props) => {
     return (
-        const {message, setMessage}    
+           
         <>
         <h1>Welcome Visitor</h1>
         <button>Subscribe</button>
