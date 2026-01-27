de0d595ec9c7fe6818fa443d03b3edb7bbb87669
ub-victor <victoireushindi371@gmail.com>
2026-01-27 23:00:33 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import Greet from './components/Greet'
 
 function App() {
 
   return (
     <>
-      <Message/>
-      <Counter/>
+      <Greet/>
     </>
   )
 }
---
+++ hello-world/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+const Greet = () => {
+    return ( 
+        <div>
+            <h1>
+                Hello {} a.k.a {}
+            </h1>
+        </div>
+     );
+}
+ 
+export default Greet;
