294110380f2e64bc8c19d221284199a2be559860
ub-victor <victoireushindi371@gmail.com>
2026-01-27 17:32:14 +0200

    Initial commit

--- hello-world/src/components/Counter.jsx
+++ hello-world/src/components/Counter.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const Counter = () => {
-    const {count, setCount} = useState(0);
+    const [count, setCount] = useState(0);
     return ( 
         <div>
             <button onClick={()=>setCount((count)=>count++)}>{count}</button>
