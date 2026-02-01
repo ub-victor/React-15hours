65511cf4ecc449ddc345d283c305f632a27985ba
ub-victor <victoireushindi371@gmail.com>
2026-02-01 14:17:24 +0200

    Initial commint

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import Greet from './components/Greet'
-import FunctionClick from './components/FunctionClick'
-
+import './index'
 function App() {
 
   return (
