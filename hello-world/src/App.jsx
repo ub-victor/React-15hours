26563af27686ae54d22230d162015cdd643e73ad
ub-victor <victoireushindi371@gmail.com>
2026-02-01 22:27:24 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
+import { useState, useEffect } from 'react'
 function App() {
 
   const Card = ({title})=>{
