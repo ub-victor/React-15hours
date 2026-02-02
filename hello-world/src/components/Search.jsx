03067d06e243bdbe413d6d337184ce44cd5fdf9a
ub-victor <victoireushindi371@gmail.com>
2026-02-02 02:29:25 +0200

    Initial commit

--- hello-world/src/components/Search.jsx
+++ hello-world/src/components/Search.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Search = ({searchTerm, setSearchTerm}) => {
   return (
-    <div className='search'></div>
+    <div className='search'>
+        <div>
+            <img src="search.svg" alt="search" />
+            <input type="text"
+            placeholder= "Search through thousand of movies"
+            value = {searchTerm}
+            ' />
+        </div>
+    </div>
   )
 }
 
