89ab9e3ac894e3ef568ce5c15caae0983c431ce8
ub-victor <victoireushindi371@gmail.com>
2026-02-02 02:08:36 +0200

    Initial commit

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <main>
       <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span>Movies</span> you'll Enjoy without the Hassle</h1>
+        </header>
+      </div>
     </main>
   )
 }
